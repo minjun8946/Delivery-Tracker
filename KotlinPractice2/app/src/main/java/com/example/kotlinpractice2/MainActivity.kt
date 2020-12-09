7e42dc9b26a36b9e47ce4169ca50a7cf84b2a2d5
minjun8946 <minjun8946@daum.net>
2020-12-09 23:40:33 +0900

    <Update> code Simplify

--- KotlinPractice2/app/src/main/java/com/example/kotlinpractice2/MainActivity.kt
+++ KotlinPractice2/app/src/main/java/com/example/kotlinpractice2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinpractice2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.*
@@ -17,22 +18,20 @@ class MainActivity : AppCompatActivity() {
     var abc :  ArrayList<DataClass>? = null
     var count = 0
     var name =0
-    lateinit var linkname: String // 링크 이름
-
-    lateinit var linknumber : String // 링크 숫자
+    var linkname = "" // 링크 이름
+    var linknumber ="" // 링크 숫자
+    lateinit var linkname1 : String
     lateinit var inter : RetrofitService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tname : EditText = findViewById(R.id.tname)
+        val tname : TextView = findViewById(R.id.tname)
         val number : EditText = findViewById(R.id.number)
         val start : Button = findViewById(R.id.start)
         val listview1 = findViewById<ListView>(R.id.listview)
         val namelistview = findViewById<ListView>(R.id.namelistview)
-        var retronum = 0
-        var namenum : Int
-        var rechange :Int = 0
-        var link : String
+        var rechange =0
+        var link = ""
 
 
 
@@ -57,85 +56,46 @@ class MainActivity : AppCompatActivity() {
                 println("wow")
 
                 check = response.body()!!
+                val UserList1 = mutableListOf<Name>()
+                for(i in 0..27) {
+                    UserList1.add(
+                            Name(check.get(i).name)
 
-
-
-                val UserList1 = arrayListOf<Name>(
-                        Name("${check?.get(0)?.name}"),Name("${check?.get(1)?.name}"),Name("${check?.get(2)?.name}"),
-                        Name("${check?.get(3)?.name}"),Name("${check?.get(4)?.name}"),Name("${check?.get(5)?.name}"),
-                        Name("${check?.get(6)?.name}"),Name("${check?.get(7)?.name}"),Name("${check?.get(8)?.name}"),
-                        Name("${check?.get(9)?.name}"),Name("${check?.get(10)?.name}"),Name("${check?.get(11)?.name}"),
-                        Name("${check?.get(12)?.name}"),Name("${check?.get(13)?.name}"),Name("${check?.get(14)?.name}"),
-                        Name("${check?.get(15)?.name}"),Name("${check?.get(16)?.name}"),Name("${check?.get(17)?.name}"),
-                        Name("${check?.get(18)?.name}"),Name("${check?.get(19)?.name}"),Name("${check?.get(20)?.name}"),
-                        Name("${check?.get(21)?.name}"),Name("${check?.get(22)?.name}"),Name("${check?.get(23)?.name}"),
-                        Name("${check?.get(24)?.name}"),Name("${check?.get(25)?.name}"),Name("${check?.get(26)?.name}"),Name("${check?.get(27)?.name}")
-                )
-                val Adapter1 = NameAdaptor(this@MainActivity,UserList1)
+                    )
+                }
+                val Adapter1 = NameAdaptor(this@MainActivity, UserList1 as ArrayList<Name>)
                 namelistview.adapter = Adapter1
 
                 namelistview.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
                     val selection = parent.getItemAtPosition(position) as Name
-                    linkname = selection.name
-                    rechange=1
+                    link = selection.name
+                    for(i in 0..27){
+                        if(link == check.get(i).name){
+                            rechange =i
+                            break;
+                        }
+                    }
+                    linkname = check.get(rechange).id
+                    tname.setText(check.get(rechange).name)
                 }
-                retronum++
 
 
             }
         })
 
-            start.setOnClickListener(View.OnClickListener {
-                val tone = tname.text.toString()
-                linknumber = number.text.toString()
 
-
-                val regionRetrofit2 = Retrofit.Builder()
-                        .baseUrl(" https://apis.tracker.delivery/carriers/" + linkname + "/tracks/" + linknumber + "/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                val server2: RetrofitService2 = regionRetrofit2.create(RetrofitService2::class.java)
-                val b = server2.getInfo2("json", "json", "json", "json", "json", "json") // 두번째 RETROFIT
-                b.enqueue(object : Callback<DataClass2> {
-                    override fun onFailure(callback: Call<DataClass2>, t: Throwable) {
-                        t.printStackTrace()
-                        println("no")
+                start.setOnClickListener(View.OnClickListener {
+                    linknumber = number.text.toString()
+                    println(linkname)
+                    println(linknumber)
+                    if(linknumber!="" && linkname !="") {
+                        val intent = Intent(this, MainActivity2::class.java)
+                       intent.putExtra("name", linkname,)
+                        intent.putExtra("number", linknumber)
+                        startActivity(intent)
                     }
-
-                    override fun onResponse(call: Call<DataClass2>, response: Response<DataClass2>) {
-                        println("wow2")
-
-                        val ab: DataClass2? = response.body()
-                        //println(response.raw()) )
-                        val arr = ab?.progresses?.count()
-                        println(ab?.state?.text)
-                        println("보내는 사람 :${ab?.from?.name}")
-                        println("받는 사람 :${ab?.to?.name}")
-
-
-                        /* for (i in 0 until arr!!) {
-                            println(ab.progresses.get(i).time)
-                            println(ab.progresses.get(i).location.name)
-                            println(ab.progresses.get(i).description)
-                        }*/
-                        val UserList = arrayListOf<User>(
-                                User("${ab?.progresses?.get(0)?.time}", "${ab?.progresses?.get(0)?.location?.name}", "${ab?.progresses?.get(0)?.description}"),
-                                User("${ab?.progresses?.get(1)?.time}", "${ab?.progresses?.get(1)?.location?.name}", "${ab?.progresses?.get(1)?.description}"),
-                                User("${ab?.progresses?.get(2)?.time}", "${ab?.progresses?.get(2)?.location?.name}", "${ab?.progresses?.get(2)?.description}"),
-                                User("${ab?.progresses?.get(3)?.time}", "${ab?.progresses?.get(3)?.location?.name}", "${ab?.progresses?.get(3)?.description}"),
-                                User("${ab?.progresses?.get(4)?.time}", "${ab?.progresses?.get(4)?.location?.name}", "${ab?.progresses?.get(4)?.description}")
-                        )
-                        val Adapter = UserAdaptor(this@MainActivity, UserList)
-                        listview1.adapter = Adapter
-                    }
-
                 })
 
-
-            })
-
     }
-
-
 }
 
--- KotlinPractice2/app/src/main/java/com/example/kotlinpractice2/MainActivity2.kt
+++ KotlinPractice2/app/src/main/java/com/example/kotlinpractice2/MainActivity2.kt
@@ -2,10 +2,69 @@ package com.example.kotlinpractice2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val linkname  = intent.getStringExtra("name")
+        val linknumber = intent.getStringExtra("number")
+        val listview1 = findViewById<ListView>(R.id.listview)
+        val stats = findViewById<TextView>(R.id.stats)
+
+        if(linkname !="" && linknumber!="") {
+            val regionRetrofit2 = Retrofit.Builder()
+                    .baseUrl("https://apis.tracker.delivery/carriers/$linkname/tracks/$linknumber/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            val server2: RetrofitService2 = regionRetrofit2.create(RetrofitService2::class.java)
+            val b = server2.getInfo2("json", "json", "json", "json", "json", "json") // 두번째 RETROFIT
+            b.enqueue(object : Callback<DataClass2> {
+                override fun onFailure(callback: Call<DataClass2>, t: Throwable) {
+                    t.printStackTrace()
+                    println("no")
+                }
+
+                override fun onResponse(call: Call<DataClass2>, response: Response<DataClass2>) {
+                    println("wow2")
+
+                    val ab: DataClass2? = response.body()
+
+                     val arr = ab?.progresses?.count()
+                    println("보내는 사람 :${ab?.from?.name}")
+                    println("받는 사람 :${ab?.to?.name}")
+                    println(arr)
+                    if(arr!=null) {
+                        stats.text = ab.state.text
+                    }
+                    if(arr==null){
+                        stats.text = "잘못된 번호입니다!"
+                    }
+
+                    val UserList = mutableListOf<User>()
+                    if (arr != null) {
+                        for(i in 0 until arr) {
+                            UserList.add(
+                                    User(ab.progresses.get(i).time, ab.progresses.get(i).location.name, ab.progresses.get(i).description)
+                            )
+                        }
+                    }
+                        val Adapter = UserAdaptor(this@MainActivity2, UserList as ArrayList<User>)
+                        listview1.adapter = Adapter
+
+
+                }
+
+            })
+        }
     }
 }
